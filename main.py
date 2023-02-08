50d8cfa2083a31377770eae81e150d5a674b7d10
jeyshp <jeyshp@gmail.com>
2023-02-08 12:52:16 -0800

    #2 New Class : Person
    New class and associated function created to gather more information about user.

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+class Person:
+  def __init__(person, name, id):
+    person.name = name
+    person.id = id
 
+Name = ""
+#global variable for name
+PersonalID = 0
+#global variable for personal id
 
 BillingPeriod = 0
 #global variable for Billing days 
 
 KwhUsed = 0
-#global variable for Kwh used  
+#global variable for Kwh used
+
+
+def createNewPerson(name, id):
+    p1 = Person(name, id)
+    return p1
+
+def printoutName(): #
+    print("Hello. Please enter your name:")
+    temp3 = input()
+    checkID(temp3.isdigit(),temp3)
+
+def printoutID(): #
+    print("Please enter your personal id number:")
+    temp2 = input()
+    checkID(temp2.isdigit(),temp2)
 
 def printoutBilling(): #prints out billing period 
     print("Please enter the number of days for this billing period:")
@@ -42,6 +65,26 @@ def checkKwh(boolcheck, temp1): #checks if input is string
         global KwhUsed
         KwhUsed = int(temp1)
 
+def checkID(boolcheck, temp1): #checks if input is string
+    if boolcheck == False:
+        print ("ONLY NUMERICAL INPUTS ARE ACCEPTED")
+        printoutID()
+    else:
+        global PersonalID
+        PersonalID = int(input())
+      
+def checkname(boolcheck, temp3): #
+    if boolcheck == True:
+        print ("NO NUMERICAL CHARACTERS" )
+        printoutName()
+    else:
+        global Name
+        Name = input()
+      
+
+printoutName()
+printoutID()
+createNewPerson(Name,PersonalID)
 printoutBilling()
 print("Thank you.")
 printoutKwh()
